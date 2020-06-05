736401aa505f61c62442d813df71349535862896
Hannah Bregman <bregmanhannah@gmail.com>
2020-06-05 00:18:54 +0000

    refactored code to include promises as opposed to 'callback hell'

---
+++ iss_promised.js
@@ -0,0 +1,26 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = function () {
+  return request('https://api.ipify.org?format=json')
+
+}
+
+const fetchCoordsByIP = function (body) {
+  let ip = JSON.parse(body).ip;
+  return request('https://ipvigilante.com/json/'+ip)
+
+}
+const fetchISSFlyOverTimes = function (body) {
+  let coords = JSON.parse(body).data;
+  return request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`)
+}
+
+const nextISSTimesForMyLocation = function (){
+  return fetchMyIP().then(fetchCoordsByIP).then(fetchISSFlyOverTimes).then((data) => {
+    const {response } = JSON.parse(data);
+    return response;
+  })
+}
+
+
+module.exports = { nextISSTimesForMyLocation };
